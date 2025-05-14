2dd05ee9a8ca38230b3bc804b75bb06463e90904
zzimog <>
2025-05-14 16:14:34 +0200

    update base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/react-calculator-app/',
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    open: true,
+  },
+});
